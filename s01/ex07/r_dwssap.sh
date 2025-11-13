a058ffffbc9dce673d3a0cf96df55a9cdfca5a53
juan <juan.utria@uac.edu.co>
2025-11-13 14:57:04 +0100

    7 corregido

--- s01/ex07/r_dwssap.sh
+++ s01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | grep -v "#" | awk -F ":" '{print $1}' | awk 'NR>1{print $0}' | awk 'NR%2{print $0}' | rev | sort | awk -v n1=$FT_LINE1 -v n2=$FT_LINE2 'NR==n1,NR==n2{print $0}' | awk '{printf $0 "_, "}' | sed s/', $'/'.'/
+cat /etc/passwd | grep -v "#" | awk -F ":" '{print $1}' | awk 'NR>1{print $0}' | awk 'NR%2{print $0}' | rev | sort -r | awk -v n1=$FT_LINE1 -v n2=$FT_LINE2 'NR==n1,NR==n2{print $0}' | awk '{printf $0 "_, "}' | sed s/', $'/'.'/
