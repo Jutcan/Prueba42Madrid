34cb7d30b915f8a459e8d2c16ae79b94fdd3990d
Jutcan <juan_utria_2000@hotmail.com>
2025-11-15 09:27:23 +0100

    5

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -1,37 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jutcan <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/15 08:40:39 by jutcan            #+#    #+#             */
+/*   Updated: 2025/11/15 09:26:16 by jutcan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
+
+void	print_write(int a, int b, int c)
+{
+	char	ascii;
+
+	ascii = '0' + a;
+	write (1, &ascii, 1);
+	ascii = '0' + b;
+	write (1, &ascii, 1);
+	ascii = '0' + c;
+	write (1, &ascii, 1);
+	if (a != 7 || b != 8 || c != 9)
+	{
+		write (1, ", ", 2);
+	}
+}
 
 void	ft_print_comb(void)
 {
-	int a;
-	int b;
-	int c;
+	int	a;
+	int	b;
+	int	c;
 
 	a = 0;
-	while (a <= 9)
+	while (a <= 7)
 	{
-		write (1, a, 4);
-		b = 0;
+		b = a + 1;
 		while (b <= 9)
 		{
-			write (1, b, 4);
-			c = 0;
+			c = b + 1;
 			while (c <= 9)
 			{
-				write (1, c, 4);
-				if (a != b && a != c && b != c)
-				{
-					
-				}
+				print_write(a, b, c);
 				c++;
 			}
 			b++;
 		}
 		a++;
 	}
-
 }
 
-void main(void)
+void	main(void)
 {
 	ft_print_comb();
 }
