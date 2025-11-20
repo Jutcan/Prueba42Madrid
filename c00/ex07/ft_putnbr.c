8a33de38bf3bed85274512d5c6ff1179b8a0f0d8
jutcan <juan_utria_2000@hotmail.com>
2025-11-20 13:40:11 +0100

    7

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -6,39 +6,51 @@
 /*   By: jutcan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 15:00:14 by jutcan            #+#    #+#             */
-/*   Updated: 2025/11/19 18:53:20 by jutcan           ###   ########.fr       */
+/*   Updated: 2025/11/20 13:39:00 by jutcan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
-void ft_putnbr(int nb)
+void	print_char_array(char ascii[], int i)
+{
+	while (i >= 0)
+	{
+		write (1, &ascii[i], 1);
+		i--;
+	}
+}
+
+void	ft_putnbr(int nb)
 {
 	char	ascii[12];
 	int		i;
+	int		sign;
 
 	i = 0;
-	while (nb > 9 || nb < -9)
+	if (nb < 0)
 	{
-		ascii[i] = (nb % 10) + '0';
+		sign = 1;
+		nb = nb * -1;
+	}
+	if (nb == 0)
+		write (1, "0", 1);
+	while (nb > 0)
+	{
+		ascii[i] = '0' + (nb % 10);
 		nb = nb / 10;
 		i++;
 	}
-	if (nb > -10 && nb < 10)
+	if (sign == 1)
 	{
-		ascii[i] = nb + '0';
+		ascii[i] = 45;
 		i++;
 	}
-
 	ascii[i] = '\0';
-	while (ascii[i--] >= 0)
-	{
-		write (1, &ascii[i], 1);
-	}
+	print_char_array(ascii, i - 1);
 }
 
 int	main(void)
 {
-	ft_putnbr(-42);
+	ft_putnbr(42);
 }
