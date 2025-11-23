1b2d224927fc3b630adbf3de75a4a43181cb329a
Jutcan <juan_utria_2000@hotmail.com>
2025-11-23 17:12:47 +0100

    1

---
+++ c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jutcan <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/23 16:26:25 by jutcan            #+#    #+#             */
+/*   Updated: 2025/11/23 17:12:21 by jutcan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+#include <stdio.h>
+int	main()
+{
+	int n;
+	int *nbr_1;
+	int **nbr_2;
+	int ***nbr_3;
+	int ****nbr_4;
+	int *****nbr_5;
+	int ******nbr_6;
+	int *******nbr_7;
+	int ********nbr_8;
+	int *********nbr_9;
+
+	nbr_1 = &n;
+	nbr_2 = &nbr_1;
+	nbr_3 = &nbr_2;
+	nbr_4 = &nbr_3;
+	nbr_5 = &nbr_4;
+	nbr_6 = &nbr_5;
+	nbr_7 = &nbr_6;
+	nbr_8 = &nbr_7;
+	nbr_9 = &nbr_8;
+
+	ft_ultimate_ft(nbr_9);
+	printf("valor de int n %d\n",n);
+	printf("contenido de puntero 9 %p\n",nbr_9);
+	printf("contenido de puntero 8 %p\n",nbr_8);
+	printf("contenido de puntero 7 %p\n",nbr_7);
+	printf("contenido de puntero 6 %p\n",nbr_6);
+	printf("contenido de puntero 5 %p\n",nbr_5);
+	printf("contenido de puntero 4 %p\n",nbr_4);
+	printf("contenido de puntero 3 %p\n",nbr_3);
+	printf("contenido de puntero 2 %p\n",nbr_2);
+	printf("contenido de puntero 1 %p\n",nbr_1);
+	printf("dirección de puntero 1 %p\n",&nbr_1);
+	printf("dirección de int n %p\n",&n);
+}*/
