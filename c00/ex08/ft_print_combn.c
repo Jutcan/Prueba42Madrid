f52d7f27a736fee8aaf2e28998bee835a52afad8
Jutcan <juan_utria_2000@hotmail.com>
2025-11-24 08:01:56 +0100

    difficult

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: jutcan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 13:44:58 by jutcan            #+#    #+#             */
-/*   Updated: 2025/11/20 17:26:02 by jutcan           ###   ########.fr       */
+/*   Updated: 2025/11/23 16:08:32 by jutcan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,10 @@
 #include <stdio.h>
 void	ft_print_combn(int n)
 {
-	char	ascii;
-	int	digit;
-	int	i;
 
-	if (n > 0 && n < 10)
-	{
-		i = 0;
-		digit = 1;
-		while (i < n)
-		{
-			digit = digit * 10;
-			i++;
-		}
-	}
-	i = 0;
-	while (i < digit)
-	{
-		printf("%d ", i);
-		i++;
-	}
 }
 
 int	main(void)
 {
-	ft_print_combn(2);
+	ft_print_combn(1);
 }
